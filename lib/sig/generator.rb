917b56b68b3998af727fadc16c506dfe79f84f3f
Ryan Cook <cookrn@gmail.com>
2013-05-01 10:57:22 -0600

    wip

--- lib/sig/generator.rb
+++ lib/sig/generator.rb
@@ -1,5 +1,3 @@
-require 'ruby_parser'
-
 require 'sig/art'
 require 'sig/expanded_code'
 require 'sig/obfuscated_code'
@@ -23,11 +21,26 @@ module Sig
       @expanded_code   = ExpandedCode.for \
                            @art.to_s,
                            @obfuscated_code.to_s
-      @artsy_code      = Ruby2Art.for \
-                           @art.to_s,
-                           @expanded_code.to_s
 
+      code = @expanded_code.to_s
+      statements = code.split ';'
+
+      map = statements[0]
       require 'pry'; binding.pry
+
+      join = statements[1]
+      ev = statements[2]
+      mapper = statements[3]
+      runner = statements[4]
+      extras = statements[5..-2]
+      starter = statements[-1]
+      require 'pry'; binding.pry
+
+      # @artsy_code      = Ruby2Art.for \
+      #                      @art.to_s,
+      #                      @expanded_code.to_s
+
+      # require 'pry'; binding.pry
 
       self
     end
