6ef67755eaac0c0baee4a9666e49e1394a6ac2d4
Ryan Cook <cookrn@gmail.com>
2013-05-03 16:42:39 -0600

    get ready to start implementing mapping

--- attempts/current_ruby_to_art.rb
+++ attempts/current_ruby_to_art.rb
@@ -1,52 +1,101 @@
 x='a={5=>" ",12=>"0",9=>"p",4=>"l",3=>"e",14=>"a",7=>"F",8=>".",13=>"r",16=>"t",0=>"$",2=>"i",15=>"d",1=>"u",6=>"s"}'
 y=[]
-y<<x[0..1]
-y<<x[2]
+t=true
+f=false
+
+y<<[x[0],f]
+y<<[x[1],t]
+y<<[x[2],t]
 
 x=~/{(.+?)}/
 between=$1
 els=between.split ','
-split_els=els.map do |e|
-  e=~/(.+?=>)(.+)/
-  [$1,"#{$2},"]
+split_els=els.reduce [] do |m,e|
+  e=~/(.+?)(=>)(.+)/
+  m << [$1,f]
+  m << [$2,t]
+  m << [$3,f]
+  m << [',',t]
+  m
 end
-y.concat split_els.flatten
-y<<x[-1]
+y.concat split_els
+y<<[x[-1],t]
+
+art = <<___
+                   iiii
+                  i::::i
+                   iiii
+    ssssssssss   iiiiiii    ggggggggg   ggggg
+  ss::::::::::s  i:::::i   g:::::::::ggg::::g
+ss:::::::::::::s  i::::i  g:::::::::::::::::g
+s::::::ssss:::::s i::::i g::::::ggggg::::::gg
+ s:::::s  ssssss  i::::i g:::::g     g:::::g
+   s::::::s       i::::i g:::::g     g:::::g
+      s::::::s    i::::i g:::::g     g:::::g
+ssssss   s:::::s  i::::i g::::::g    g:::::g
+s:::::ssss::::::si::::::ig:::::::ggggg:::::g
+s::::::::::::::s i::::::i g::::::::::::::::g
+ s:::::::::::ss  i::::::i  gg::::::::::::::g
+  sssssssssss    iiiiiiii    gggggggg::::::g
+                                     g:::::g
+                         gggggg      g:::::g
+                         g:::::gg   gg:::::g
+                          g::::::ggg:::::::g
+                           gg:::::::::::::g
+                             ggg::::::ggg
+                                ggggg
+___
+
+new_art = ''
+
+art.lines.each do | line |
+  new_line =
+    line.gsub /\S+/ do | match |
+      char = '!'
+      new_str = ''
+      match.size.times do
+        new_str << char
+      end
+      new_str
+    end
+
+  new_art << new_line
+end
 
 require 'pry'; binding.pry
 
 __END__
 
-a=
+a        =
 {
-5=>
-" ",
-12=>
-"0",
-9=>
-"p",
-4=>
-"l",
-3=>
-"e",
-14=>
-"a",
-7=>
-"F",
-8=>
-".",
-13=>
-"r",
-16=>
-"t",
-0=>
-"$",
-2=>
-"i",
-15=>
-"d",
-1=>
-"u",
-6=>
+5        =>
+" "      ,
+12       =>
+"0"      ,
+9        =>
+"p"      ,
+4        =>
+"l"      ,
+3        =>
+"e"      ,
+14       =>
+"a"      ,
+7        =>
+"F"      ,
+8        =>
+"."      ,
+13       =>
+"r"      ,
+16       =>
+"t"      ,
+0        =>
+"$"      ,
+2        =>
+"i"      ,
+15       =>
+"d"      ,
+1        =>
+"u"      ,
+6        =>
 "s"
 }
